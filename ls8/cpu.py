03acb07e4d65b027bdad935cc3cb1146160b99d7
Jason Loomis <jason@jrloomis.com>
2020-04-20 19:14:02 -0400

    day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,16 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+
+        self.ram = [0] * 256  # ? memory
+        self.reg = [0] * 8  # ? registers
+        self.pc = 0  # ? program counter
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +22,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -46,20 +49,52 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(
+            f"TRACE: %02X | %02X %02X %02X |"
+            % (
+                self.pc,
+                # self.fl,
+                # self.ie,
+                self.ram_read(self.pc),
+                self.ram_read(self.pc + 1),
+                self.ram_read(self.pc + 2),
+            ),
+            end="",
+        )
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.reg[i], end="")
 
         print()
 
+    # ? takes RAM address, returns stored value
+    def ram_read(self, addr):
+        return self.ram[addr]
+
+    # ? takes address & value, writes value to address in RAM
+    def ram_write(self, addr, val):
+        self.ram[addr] = val
+
     def run(self):
         """Run the CPU."""
-        pass
+
+        running = True
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        while running:
+            instruction = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if instruction == LDI:
+                # print(f"op a: {operand_a} | op b: {operand_b}")
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif instruction == PRN:
+                print(f"prints {self.reg[operand_a]}")
+                self.pc += 2
+            elif instruction == HLT:
+                # print("stop")
+                running = False
